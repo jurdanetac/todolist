6a4c4a3091840bba3a92434603178d5281cc9fe8
Juan Urdaneta <jurdanetac10@gmail.com>
2022-08-24 20:32:48 -0400

    Implement buttons functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,37 @@
-console.info("JavaScript loaded");
+const itemList = document.getElementById('itemList');
+const addBtn = document.getElementById('addBtn');
+const delBtn = document.getElementById('delBtn');
+const clrBtn = document.getElementById('clrBtn');
+
+const noItemsAlert = document.getElementById('noItemsAlert');
+
+const toggleItemsAlert = () => {
+  if (itemList.childElementCount === 0) {
+    noItemsAlert.style.display = 'block';
+    return undefined;
+  }
+
+  noItemsAlert.style.display = 'none';
+};
+
+addItem = addBtn.onclick = () => {
+  const item = document.createElement('li');
+  item.innerText = `Item ${itemList.children.length}`;
+  itemList.appendChild(item);
+  toggleItemsAlert();
+};
+
+removeItem = delBtn.onclick = () => {
+  if (itemList.children.length) {
+    const last = itemList.children[itemList.children.length - 1];
+    itemList.removeChild(last);
+    toggleItemsAlert();
+  }
+};
+
+clearItems = clrBtn.onclick = () => {
+  itemList.innerHTML = '';
+  toggleItemsAlert();
+};
+
+console.info('JavaScript loaded');
